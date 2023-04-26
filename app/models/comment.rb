6ff40c48f974f021d64a2aea7eaed6cb28573471
santo <rbsanto@proton.me>
2023-04-26 20:31:14 +0200

    Better styling

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :video
 
-  validates :commenter, presence: true
+  validates :commenter, presence: true, length: { maximum: 28 }
   validates :body, presence: true, length: { minimum: 2, maximum: 300 }
 end
