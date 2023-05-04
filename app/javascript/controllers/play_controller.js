77f7a4ca064f56f5f69dd4d914002a4c5f3f1233
santo <rbsanto@proton.me>
2023-05-04 20:36:42 +0200

    Added tooltip svg icon on video and thumbnail

--- app/javascript/controllers/play_controller.js
+++ app/javascript/controllers/play_controller.js
@@ -3,6 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     const media = document.querySelector('video');
-    media.addEventListener('canplaythrough', (event) => { media.play();});
+    media.addEventListener('canplaythrough', () => { media.play();});
   }
 }
