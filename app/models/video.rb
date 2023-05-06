57914d38b8080fe0f8b1cb3e0451ea6752f8dd53
santo <rbsanto@proton.me>
2023-05-06 20:29:20 +0200

    Added js validation for file size + animations

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,40 +1,40 @@
 class Video < ApplicationRecord
-    has_one_attached :clip
-    has_one_attached :thumbnail
-    has_many :comments, dependent: :destroy
+  has_one_attached :clip
+  has_one_attached :thumbnail
+  has_many :comments, dependent: :destroy
 
-    validates :title, presence: true, uniqueness: true, length: { maximum: 40 }
-    validates :description, length: { maximum: 550}
-    validates :clip, presence: true
-    validates :thumbnail, presence: true
-    validate :correct_video_type
-    validate :correct_image_type
-    validate :correct_clip_size
-    validate :correct_image_size
+  validates :title, presence: true, uniqueness: true, length: { maximum: 40 }
+  validates :description, length: { maximum: 550}
+  validates :clip, presence: true
+  validates :thumbnail, presence: true
+  validate :correct_video_type
+  validate :correct_image_type
+  validate :correct_clip_size
+  validate :correct_image_size
 
-    private
+  private
 
-    def correct_video_type
-        if clip.attached? && !clip.content_type.in?(%w(video/mp4 video/webm))
-            errors.add(:clip, "must be a MP4 or WEBM")
-        end
+  def correct_video_type
+    if clip.attached? && !clip.content_type.in?(%w(video/mp4 video/webm))
+        errors.add(:clip, "must be a MP4 or WEBM")
     end
+  end
 
-    def correct_image_type
-        if thumbnail.attached? && !thumbnail.content_type.in?(%w(image/jpeg image/png))
-            errors.add(:thumbnail, "must be a JPEG or PNG")
-        end
+  def correct_image_type
+    if thumbnail.attached? && !thumbnail.content_type.in?(%w(image/jpeg image/png))
+        errors.add(:thumbnail, "must be a JPEG or PNG")
     end
+  end
 
-    def correct_clip_size
-        if clip.attached? && clip.byte_size > 150.megabytes
-            errors.add(:clip, "should be less than 150MB")
-        end
+  def correct_clip_size
+    if clip.attached? && clip.byte_size > 150.megabytes
+        errors.add(:clip, "should be less than 150MB")
     end
+  end
 
-    def correct_image_size
-        if thumbnail.attached? && thumbnail.byte_size > 2.megabytes
-            errors.add(:thumbnail, "should be less than 2MB")
-        end
+  def correct_image_size
+    if thumbnail.attached? && thumbnail.byte_size > 2.megabytes
+        errors.add(:thumbnail, "should be less than 2MB")
     end
+  end
 end
