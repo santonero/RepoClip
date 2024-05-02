e723a081b99a14b660c57e78459895a36609a6d5
Santonero <rbsanto@proton.me>
2024-05-02 21:08:22 +0200

    Ruby upgrade (3.3.1)

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,5 +4,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers", preload: false
 pin "vjs"
-pin "video.js", to: "https://unpkg.com/video.js@8.10.0/dist/video.min.js"
-pin "emoji-mart", to: "https://unpkg.com/emoji-mart@5.5.2/dist/browser.js"
+pin "video.js", to: "https://unpkg.com/video.js@8.12.0/dist/video.min.js"
+pin "emoji-mart", to: "https://unpkg.com/emoji-mart@5.6.0/dist/browser.js"
